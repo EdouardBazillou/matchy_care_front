e1fd4771f8842c63af08af80a62cf9ff2ede78cf
EdouardBazillou <edouard.bazillou@gmail.com>
2023-04-17 10:24:05 +0200

    router commit

---
+++ src/components/error.js
@@ -0,0 +1,8 @@
+function Error() {
+  return (
+    <div>
+      <h2>Error</h2>
+    </div>
+  );
+}
+export default Error;
---
+++ src/users/Accueil.js
@@ -0,0 +1,8 @@
+function Accueil() {
+  return (
+    <div>
+      <h1>Matchy Care</h1>
+    </div>
+  );
+}
+export default Accueil;
