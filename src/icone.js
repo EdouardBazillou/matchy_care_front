cb83a4a5746145db37d243d7ad0e2d18e621b917
EdouardBazillou <edouard.bazillou@gmail.com>
2023-04-18 10:52:04 +0200

    header update

--- src/icone.js
+++ src/icone.js
@@ -1,22 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
-import {FaAccessibleIcon,FaBath,FaToilet,FaUserFriends}from "react-icons/fa";
-import {MdSoupKitchen,MdOutlineDirectionsCarFilled,MdFitnessCenter,MdOutlineCleaningServices,MdOutlineMedication,MdOutlineHealthAndSafety} from "react-icons/md";
-function icone() {
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  FaAccessibleIcon,
+  FaBath,
+  FaToilet,
+  FaUserFriends,
+} from "react-icons/fa";
+import {
+  MdSoupKitchen,
+  MdOutlineDirectionsCarFilled,
+  MdFitnessCenter,
+  MdOutlineCleaningServices,
+  MdOutlineMedication,
+  MdOutlineHealthAndSafety,
+} from "react-icons/md";
+function Icone() {
   return (
-<div className="App">
- <div className="icon-text-container"></div>
- <p><a href="#"onClick><FaAccessibleIcon/></a>mobilite</p>
- <p><a href="2"onClick><MdSoupKitchen/></a>repas</p>
- <p><a href="3"onClick><MdOutlineCleaningServices/></a>Entretient</p>
- <p><a href="5"onClick><FaToilet/></a>Change</p>
- <p><a href="4"onClick><MdFitnessCenter/></a>Réeducation</p>
- <p><a href="2"onClick><FaBath/></a>Toilette</p>
- <p><a href="3"onClick><MdOutlineHealthAndSafety/></a>Soins</p>
- <p><a href="1"onClick><MdOutlineMedication/></a>Traitement</p>
- <p><a href="9"onClick><FaUserFriends/></a>Compagnie</p>
- <p><a href="o"onClick><MdOutlineDirectionsCarFilled/></a>Transport</p>
-  </div>
+    <div className="App">
+      <div className="icon-text-container"></div>
+      <p>
+        <a href="#" onClick>
+          <FaAccessibleIcon />
+        </a>
+        mobilite
+      </p>
+      <p>
+        <a href="2" onClick>
+          <MdSoupKitchen />
+        </a>
+        repas
+      </p>
+      <p>
+        <a href="3" onClick>
+          <MdOutlineCleaningServices />
+        </a>
+        Entretient
+      </p>
+      <p>
+        <a href="5" onClick>
+          <FaToilet />
+        </a>
+        Change
+      </p>
+      <p>
+        <a href="4" onClick>
+          <MdFitnessCenter />
+        </a>
+        Réeducation
+      </p>
+      <p>
+        <a href="2" onClick>
+          <FaBath />
+        </a>
+        Toilette
+      </p>
+      <p>
+        <a href="3" onClick>
+          <MdOutlineHealthAndSafety />
+        </a>
+        Soins
+      </p>
+      <p>
+        <a href="1" onClick>
+          <MdOutlineMedication />
+        </a>
+        Traitement
+      </p>
+      <p>
+        <a href="9" onClick>
+          <FaUserFriends />
+        </a>
+        Compagnie
+      </p>
+      <p>
+        <a href="o" onClick>
+          <MdOutlineDirectionsCarFilled />
+        </a>
+        Transport
+      </p>
+    </div>
   );
 }
-export default icone;
+export default Icone;
--- src/users/Accueil.js
+++ src/users/Accueil.js
@@ -3,31 +3,39 @@ import BurgerMenu from "../components/burgerMenu";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import Icone from "../icone";
 
 function Accueil() {
   return (
-    <div className="home">
-      <div className="title">
-        <h1>
-          <p>Matchy</p>
-          <p className="p2">Care</p>
-        </h1>
-      </div>
-      <div className="title2">
-        <h1>Réservez votre auxiliaire de vie idéal(e)</h1>
-      </div>
-      <div className="right">
-        <div className="title3">
-          <p>Devenir auxiliaire de vie </p>
+    <div>
+      <div className="home">
+        <div className="title">
+          <h1>
+            <p>Matchy</p>
+            <p className="p2">Care</p>
+          </h1>
         </div>
-        <div className="icon1">
-          <BurgerMenu />
-          <div className="icon2">
-            <Link to="/profile" className="profilLink">
-              <FontAwesomeIcon icon={faUser} />{" "}
-            </Link>
+        <div className="title2">
+          <h1>Réservez votre auxiliaire de vie idéal(e)</h1>
+        </div>
+        <div className="right">
+          <div className="title3">
+            <p>Devenir auxiliaire de vie </p>
+          </div>
+          <div className="icon1">
+            <BurgerMenu />
+            <div className="icon2">
+              <Link to="/profile" className="profilLink">
+                <FontAwesomeIcon icon={faUser} />{" "}
+              </Link>
+            </div>
           </div>
         </div>
+      </div>
+      <br></br>
+      <br></br>
+      <div className="icone">
+        <Icone />
       </div>
     </div>
   );
