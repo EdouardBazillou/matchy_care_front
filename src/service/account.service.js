0e21ae5277c4ae2812864c8ffec6b4aef73440cb
ahamed <ahmeddu012@gmail.com>
2023-04-20 13:42:05 +0200

    login.push

---
+++ src/service/account.service.js
@@ -0,0 +1,22 @@
+let saveToken = (token) => {
+localStorage.setItem('token',token)
+}
+
+let logout = () => {
+    localStorage.removeItem('token')
+}
+
+let islogged = () => {
+
+  let token = localStorage.getItem('token')
+
+
+  return !!token
+
+}
+
+export const accountService = {
+
+saveToken, logout, islogged
+
+}
--- src/users/login.js
+++ src/users/login.js
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { accountService } from "../service/account.service";
 
 function Login() {
   const [user, setUser] = useState({
@@ -38,6 +39,7 @@ function Login() {
       })
       .then((response) => {
         console.log("response", response);
+         accountService.savetoken(response.data.access_token)
         if ((response.success = true)) {
           navigate("/");
         }
@@ -54,6 +56,7 @@ function Login() {
       </div>
       <form onSubmit={handleSubmit} className="loginForm">
         <div>
+   <h5>Bienvenue Sur Matchy Care</h5>
           <input
             type="text"
             className="Email"
