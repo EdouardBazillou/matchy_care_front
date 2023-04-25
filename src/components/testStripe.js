e900dac6f692386eb581e6c304579f79b53163ae
PAROLDI-ALICE <aliceparoldi@gmail.com>
2023-04-25 14:32:15 +0200

    Create Stripe CheckoutForm & Test payment

---
+++ src/components/testStripe.js
@@ -0,0 +1,12 @@
+import react from "react";
+import StripeContainer from "../stripe/stripeContainer";
+
+function TestStripe() {
+  return (
+    <div>
+      <h1>test paiement Stripe développeur</h1>
+      <StripeContainer />
+    </div>
+  );
+}
+export default TestStripe;
