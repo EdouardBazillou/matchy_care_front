717fdd74fbe48f5afedb5daad9d180a7e92444f6
PAROLDI-ALICE <aliceparoldi@gmail.com>
2023-04-28 09:32:53 +0200

    Updates Stripe function

--- src/components/testStripe.js
+++ src/components/testStripe.js
@@ -1,11 +1,16 @@
 import react from "react";
 import StripeContainer from "../stripe/stripeContainer";
+import "../stripe/stripeForm.css";
 
 function TestStripe() {
   return (
-    <div>
-      <h1>test paiement Stripe développeur</h1>
-      <StripeContainer />
+    <div className="paymentForm">
+      <div className="titreTestStripe">
+        <h2>Paiement test pour Matchy Care</h2>
+      </div>
+      <div className="formStripe">
+        <StripeContainer />
+      </div>
     </div>
   );
 }
